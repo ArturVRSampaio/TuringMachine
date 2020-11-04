9e10a3f23ea8a36f21d3f634cabf7987ccb2a46c
ArturVRSampaio <artur.vitorio@astisoftware.com.br>
2020-11-03 23:12:47 -0300

    estrutura padrao view

--- app/Views/Painel_Controle_View.php
+++ app/Views/Painel_Controle_View.php
@@ -1,1 +1,23 @@
-ola mundo
+
+<?php
+//Incluindo Header
+$this->load->view('Base_View.php');
+?>
+
+
+
+
+<script type= 'text/javascript'>
+
+$(document).ready(function () {
+
+}
+
+
+</script>
+
+
+<?php
+//Incluindo Rodapé
+//$this->load->view('rodape.php');
+?>
