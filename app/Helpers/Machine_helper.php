5ec885a214a1f70fc0ec7192b9e6ba53ed1904f6
ArturVRSampaio <artur.vitorio@astisoftware.com.br>
2020-11-08 16:41:55 -0300

    regra geral de codificacao dos estados da maquina

--- app/Helpers/Machine_helper.php
+++ app/Helpers/Machine_helper.php
@@ -18,14 +18,34 @@
 //
 function master()
 {
-  bool $fim = FALSE;
-  array $fita = [];
-  array $estados = [];
+  //flag de aceita
+  $fim = True;
+  //fita de memoria, originalmente contendo a entrada do usuario
+  $fita = [];
+
+
+  //lista de estados do usuario
+  $estados = [];
+
+  //ler
+  //
+  //se 0 escreva //se 0 mova //se 0 muda p estado // //se 1 escreva //se 1 mova // muda p estado
+  //
+  //exemplo 
+  //{2, -2, 3, 0, 2, 3}
+  //ou seja
+  //se 0 escreva nada, mova 2 p a esquerda mude para o estado 3
+  //se 1 escreva 0, mova 2 p a direita mude para o estado 3
+
 
   while($fim){
-      $estados[x]
-  }
+      $estados[x];
+
+    };
 }
+
+
+
 function move_right()
 {
 
