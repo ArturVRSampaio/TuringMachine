b594f912c27337dff5e676573999e02159300829
ArturVRSampaio <artur.vitorio@astisoftware.com.br>
2020-11-07 22:45:14 -0300

    alterando title

--- app/Views/Base_View.php
+++ app/Views/Base_View.php
@@ -4,10 +4,10 @@
 <head>
 
 <meta charset="UTF-8">
-<title>Welcome to CodeIgniter 4!</title>
+<title>TuringMachine</title>
 <meta name="description" content="The small framework with powerful features">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
-<link rel="shortcut icon" type="image/png" href="/favicon.ico"/>
+<link rel="shortcut icon" type="image/png" href="/favicon.png"/>
 
 
 <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
