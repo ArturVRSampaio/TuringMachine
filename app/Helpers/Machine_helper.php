a25a42eb6199c353b40199a87d8c40e61a55a4b0
ArturVRSampaio <artur.vitorio@astisoftware.com.br>
2020-11-09 03:34:49 -0300

    its alive, a maquina finalmente funciona

--- app/Helpers/Machine_helper.php
+++ app/Helpers/Machine_helper.php
@@ -1,130 +1,68 @@
-<?php 
-
-// competencias de uma MQT:
-//
-//shift left
-//shift left
-//
-//write
-//read
-//
-//process
-//
-
-
-  function master()
-  {
-  //flag de aceita
-  $fim = True;
-
-  //alfabeto da fita (conjunto finito de símbolos)
-  //fita de memoria, originalmente contendo a entrada do usuario
-  $fita = [];
-
-  //alfabeto finito de símbolos
-  $alfabeto = [];
+<?php
+function master(){
   
   //FQ de estados finais
   $estadosFinais = [];
-  
-  //símbolo branco
 
   //conjunto finito de estados
   //lista de estados do usuario
   $estados = [];
 
+  //alfabeto da fita (conjunto finito de símbolos)
+  //fita de memoria, originalmente contendo a entrada do usuario
+  //$fita=["a","b",""];
+  $fita=array_fill(0, 100000, '');
+  //alfabeto finito de símbolos
+  $alfabeto=["a","b",""];
+  
+  //símbolo branco
+  //""
+  
+  //q0
+  // 000 -> Q0a a 2 1
+  // 010 -> Q0b a 2 1
+  // 020 -> Q0_ a 2 0
+  
+  //q1
+  // 100 -> Q1a a 2 1
+  // 110 -> Q1b a 2 1
+  // 120 -> Q1_ a 2 0
 
-  //para garantir que o conjunto de estados seja ligado a qualquer alfabeto de qualquer tamanho 
-  //
-  //funcionamento:
-  //
-  //alfabeto: {a,b,c,d}
-  //
-  //estados:{0,1,2,3,4}
-  //
-  //estado[0]:{a,b,c,d}
-  //
-  //estado[0,a]:{ escreve X, move Y, muda para o estado[Z]}
 
-  //fixando valores de teste
-  //
-  $fita=["a","b","c","d"];
-  // 
-  $alfabeto=["a","b","c","d",""];
-  
-  
-  //example
-  /////////////////////////// 
-  //      || 0  1 ||       ||
-  //a 2 1 || a  a || a 2 1 ||
-  //a 2 1 || b  b || a 2 1 ||
-  //a 2 1 || c  c || a 2 1 ||
-  //a 2 1 || d  d || a 2 1 ||
-  //a 2 1 || "" ""|| a 2 1 ||
-  ///////////////////////////
-  
-  
-  $estados  = ["0","1"];
-  $estados[0] = ["a","b","c","d",""];
-  $estados[1] = ["a","b","c","d",""];
-  
-  $estados[0]["a"] = ["a","2","1"];
-  $estados[0]["b"] = ["a","2","1"];
-  $estados[0]["c"] = ["a","2","1"];
-  $estados[0]["d"] = ["a","2","1"];
-  $estados[0][""] = ["a","2","1"];
-  
 
-  $estados[1]["a"] = ["a","2","1"];
-  $estados[1]["b"] = ["a","2","1"];
-  $estados[1]["c"] = ["a","2","1"];
-  $estados[1]["d"] = ["a","2","1"];
-  $estados[1][""] = ["a","2","1"];
+//array exemplo 
+  $listaAcaoa = array("a", "a", 2, 1);
+  $listaAcaob = array("b", "a", 2, 1);
+  $listaAcaoVazio = array("", "a", 2, 0);
+  $listachave = array($listaAcaoa, $listaAcaob, $listaAcaoVazio);
+  $estados = array($listachave, $listachave);
+
+
   
+  //valores iniciais
   $aceita = False;
   $estadoAtual = 0;
-  $fitaPos = 0;
+  $fitaPos = 50000;
+  $fim = false;
+  $leitura = $fita[$fitaPos];
 
   while($fim!= true){
     //ler conteudo atual da lista
     $leitura = $fita[$fitaPos];
 
     //verificando existe algum valor do alfabeto do estado atual
-    if(array_search($leitura, $estados[$estadoAtual])){
-      //identificando proxima tarefa
-      $acoes = $estados[$estadoAtual][$fitaPos];
-    }
-    else{
-      //execucao encerrada em estado nao final
-      $aceita=False;
-      $fim=True;
-    }
+    foreach($estados[$estadoAtual] as $key => $value){
+      if($value[0] ===$leitura){
 
-    $fita[$fitaPos]+= $acoes[1];
-    $fitaPos+= $acoes[2];
-    
-    if($acoes[3]<0){
-      //execucao encerrada em estado final
-      $aceita=True;
-      $fim=True;
-    }
-    else{
-      $estadoAtual= $acoes[3];
-    }
-
-    ////possiveis fins de execucao:
-    //// fim esperado, aceitacao
-    //// fim inesperado, nao aceitacao/ erro
-    ////execucao encerrada em estado final
-    //if(false){
-    //  $aceita=True;
-    //  $fim=True;
-    //}
-    ////execucao encerrada em estado nao final
-    //if(false){
-    //  $aceita=False;
-    //  $fim=True;
-    //}
+        //escrevendo
+        $fita[$fitaPos] = $estados[$estadoAtual][$key][1];
+        //movendo
+        $fitaPos += $estados[$estadoAtual][$key][2];
+                
+        //mudando estado
+        $estadoAtual = $estados[$estadoAtual][$key][3];
+      }
+    }  
   };
-  var_dump($fita);
+  return($fita);
 }
