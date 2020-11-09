2073c8dd6f1acdf9527bfb8ab0695a6acce57daf
ArturVRSampaio <artur.vitorio@astisoftware.com.br>
2020-11-09 00:03:25 -0300

    criando base da maquina

--- app/Helpers/Machine_helper.php
+++ app/Helpers/Machine_helper.php
@@ -50,55 +50,81 @@
   //
   $fita=["a","b","c","d"];
   // 
-  $alfabeto=["a","b"];
+  $alfabeto=["a","b","c","d",""];
   
   
-  //
+  //example
+  /////////////////////////// 
+  //      || 0  1 ||       ||
+  //a 2 1 || a  a || a 2 1 ||
+  //a 2 1 || b  b || a 2 1 ||
+  //a 2 1 || c  c || a 2 1 ||
+  //a 2 1 || d  d || a 2 1 ||
+  //a 2 1 || "" ""|| a 2 1 ||
+  ///////////////////////////
+  
+  
   $estados  = ["0","1"];
-  $estados["0"] = ["a","b","c","d"];
-  $estados["1"] = ["a","b","c","d"];
+  $estados[0] = ["a","b","c","d",""];
+  $estados[1] = ["a","b","c","d",""];
   
-  $estados["0"]["a"] = ["a","2","1"];
-  $estados["0"]["b"] = ["a","2","1"];
-  $estados["0"]["c"] = ["a","2","1"];
-  $estados["0"]["d"] = ["a","2","1"];
+  $estados[0]["a"] = ["a","2","1"];
+  $estados[0]["b"] = ["a","2","1"];
+  $estados[0]["c"] = ["a","2","1"];
+  $estados[0]["d"] = ["a","2","1"];
+  $estados[0][""] = ["a","2","1"];
   
 
-  $estados["1"]["a"] = ["a","2","1"];
-  $estados["1"]["b"] = ["a","2","1"];
-  $estados["1"]["c"] = ["a","2","1"];
-  $estados["1"]["d"] = ["a","2","1"];
+  $estados[1]["a"] = ["a","2","1"];
+  $estados[1]["b"] = ["a","2","1"];
+  $estados[1]["c"] = ["a","2","1"];
+  $estados[1]["d"] = ["a","2","1"];
+  $estados[1][""] = ["a","2","1"];
   
-  
+  $aceita = False;
   $estadoAtual = 0;
   $fitaPos = 0;
-  //while($fim!= true){
 
-    //posicao atual na fita
+  while($fim!= true){
+    //ler conteudo atual da lista
     $leitura = $fita[$fitaPos];
+
+    //verificando existe algum valor do alfabeto do estado atual
+    if(array_search($leitura, $estados[$estadoAtual])){
+      //identificando proxima tarefa
+      $acoes = $estados[$estadoAtual][$fitaPos];
+    }
+    else{
+      //execucao encerrada em estado nao final
+      $aceita=False;
+      $fim=True;
+    }
+
+    $fita[$fitaPos]+= $acoes[1];
+    $fitaPos+= $acoes[2];
     
-    //identificando prioxima tarefa
-    $acoes = $estados[$estadoAtual][$leitura];
-  
-    var_dump($acoes);
-  //};
-}
+    if($acoes[3]<0){
+      //execucao encerrada em estado final
+      $aceita=True;
+      $fim=True;
+    }
+    else{
+      $estadoAtual= $acoes[3];
+    }
 
-
-
-function move_right()
-{
-
-}
-function move_left()
-{
-  
-}
-function write()
-{
-  
-}
-function read()
-{
-  
+    ////possiveis fins de execucao:
+    //// fim esperado, aceitacao
+    //// fim inesperado, nao aceitacao/ erro
+    ////execucao encerrada em estado final
+    //if(false){
+    //  $aceita=True;
+    //  $fim=True;
+    //}
+    ////execucao encerrada em estado nao final
+    //if(false){
+    //  $aceita=False;
+    //  $fim=True;
+    //}
+  };
+  var_dump($fita);
 }
