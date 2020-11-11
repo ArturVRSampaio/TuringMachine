fb7c891b5c6326111afbb438e11ff2b5315f515f
Reinaldo Rozato Junior <naldoepingo1@gmail.com>
2020-11-10 23:36:19 -0300

    center images

--- app/Views/Painel_Controle_View.php
+++ app/Views/Painel_Controle_View.php
@@ -6,8 +6,9 @@
 <body>
 
 <!-- CONTENT -->
-
-
+<div class= mt-4>
+    <?= $this->include('Tabs/Painel_controle_top') ?>
+</div>
 
 <div class="container-fluid mt-4">
 	<div class="row">
@@ -38,103 +39,6 @@
 		</div>
 	</div>
 </div>
-
-
-<div class="container-fluid mb-4">
-	<div class="row">
-		<div class="col-md-1">
-		</div>
-		<div class="col-md-10">
-			<div class="row">
-				
-				
-				<div class="col-md-6">
-
-					<h3>
-						Saida de dados
-					</h3>
-                    <br>
-                    <h3>
-						Maquina
-					</h3>
-					<dl>
-						<dt>
-							Description lists
-						</dt>
-						<dd>
-							A description list is perfect for defining terms.
-						</dd>
-						<dt>
-							Euismod
-						</dt>
-						<dd>
-							Vestibulum id ligula porta felis euismod semper eget lacinia odio sem nec elit.
-						</dd>
-						<dd>
-							Donec id elit non mi porta gravida at eget metus.
-						</dd>
-						<dt>
-							Malesuada porta
-						</dt>
-						<dd>
-							Etiam porta sem malesuada magna mollis euismod.
-						</dd>
-						<dt>
-							Felis euismod semper eget lacinia
-						</dt>
-						<dd>
-							Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.
-						</dd>
-					</dl>
-				</div>
-
-
-
-				<div class="col-md-6 mt-5">
-                <br>
-					<h3>
-						Estados
-					</h3>
-					<dl>
-						<dt>
-							Description lists
-						</dt>
-						<dd>
-							A description list is perfect for defining terms.
-						</dd>
-						<dt>
-							Euismod
-						</dt>
-						<dd>
-							Vestibulum id ligula porta felis euismod semper eget lacinia odio sem nec elit.
-						</dd>
-						<dd>
-							Donec id elit non mi porta gravida at eget metus.
-						</dd>
-						<dt>
-							Malesuada porta
-						</dt>
-						<dd>
-							Etiam porta sem malesuada magna mollis euismod.
-						</dd>
-						<dt>
-							Felis euismod semper eget lacinia
-						</dt>
-						<dd>
-							Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.
-						</dd>
-					</dl>
-				</div>
-			
-			
-			</div>
-		</div>
-		<div class="col-md-1">
-		</div>
-	</div>
-</div>
-
-
 
 <?= $this->include('Footer') ?>
 
