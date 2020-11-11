3d90bf9ed9a601a169bbbc7aa2255c2aece8e468
ArturVRSampaio <artur.vitorio@astisoftware.com.br>
2020-11-10 22:40:45 -0300

    ajustando tabs

--- app/Views/Home_View.php
+++ app/Views/Home_View.php
@@ -11,13 +11,13 @@
 		<a class="nav-link active" data-toggle="tab" href="#home">Home</a>
 	</li>
 	<li class="nav-item">
-		<a class="nav-link" data-toggle="tab" href="#menu1">Tutorial</a>
+		<a class="nav-link" data-toggle="tab" href="#menu1">menu1</a>
 	</li>
 	<li class="nav-item">
-		<a class="nav-link" data-toggle="tab" href="#menu2">Descrição técnica</a>
+		<a class="nav-link" data-toggle="tab" href="#menu2">menu2</a>
 	</li>
 	<li class="nav-item">
-		<a class="nav-link" data-toggle="tab" href="#menu3">Menu 3</a>
+		<a class="nav-link" data-toggle="tab" href="#menu3">menu3</a>
 	</li>
 </ul>
 
@@ -117,19 +117,13 @@
 		
 	</div>
     <div id="menu1" class="tab-pane fade">
-      <div>
-	  	
-	  </div>
+			<h3>Menu 1</h3>
+	      <p>Eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.</p>
     </div>
-    <div id="menu2" class="tab-pane fade">
-      <div>
-		<div>
-				<div class= mt-5>
-
-
-				</div>
-			</div>
-	  </div>
+	
+	<div id="menu2" class="tab-pane fade">
+	      <h3>Menu 2</h3>
+	      <p>Eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.</p>
     </div>
     <div id="menu3" class="tab-pane fade">
       <h3>Menu 3</h3>
