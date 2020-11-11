30d85a07ca1077d7109587ca2982163d67d0d83b
ArturVRSampaio <artur.vitorio@astisoftware.com.br>
2020-11-10 23:56:54 -0300

    removendo texto

--- app/Views/Painel_Controle_View.php
+++ app/Views/Painel_Controle_View.php
@@ -52,82 +52,20 @@
 			<div class="row">
 				
 				
-				<div class="col-md-6">
+				<div class="col-md-6 mt-5">
 
 					<h3>
 						Saida de dados
 					</h3>
-                    <br>
-                    <h3>
-						Maquina
-					</h3>
-					<dl>
-						<dt>
-							Description lists
-						</dt>
-						<dd>
-							A description list is perfect for defining terms.
-						</dd>
-						<dt>
-							Euismod
-						</dt>
-						<dd>
-							Vestibulum id ligula porta felis euismod semper eget lacinia odio sem nec elit.
-						</dd>
-						<dd>
-							Donec id elit non mi porta gravida at eget metus.
-						</dd>
-						<dt>
-							Malesuada porta
-						</dt>
-						<dd>
-							Etiam porta sem malesuada magna mollis euismod.
-						</dd>
-						<dt>
-							Felis euismod semper eget lacinia
-						</dt>
-						<dd>
-							Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.
-						</dd>
-					</dl>
+
 				</div>
 
 
 
 				<div class="col-md-6 mt-5">
-                <br>
 					<h3>
 						Estados
 					</h3>
-					<dl>
-						<dt>
-							Description lists
-						</dt>
-						<dd>
-							A description list is perfect for defining terms.
-						</dd>
-						<dt>
-							Euismod
-						</dt>
-						<dd>
-							Vestibulum id ligula porta felis euismod semper eget lacinia odio sem nec elit.
-						</dd>
-						<dd>
-							Donec id elit non mi porta gravida at eget metus.
-						</dd>
-						<dt>
-							Malesuada porta
-						</dt>
-						<dd>
-							Etiam porta sem malesuada magna mollis euismod.
-						</dd>
-						<dt>
-							Felis euismod semper eget lacinia
-						</dt>
-						<dd>
-							Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.
-						</dd>
-					</dl>
 				</div>
 			
 			
