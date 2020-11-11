3808d4eb78f693d13f1ba103db7685875df7effc
ArturVRSampaio <artur.vitorio@astisoftware.com.br>
2020-11-11 10:59:35 -0300

    reajustando nav

--- app/Views/NavBar.php
+++ app/Views/NavBar.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <!-- NavBar-->
 <body class="col-md-12 clearfix">
-<div class="col-md-12">
+<div class="mw-100 p-0 m-0">
 	<div class="row">
 		<div class="col-md-12">
 			<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
