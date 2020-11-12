c4d09676453504e605e2855fb825e67c76b98947
index325 <gabriel.moraes.o@hotmail.com>
2020-11-12 01:23:15 -0300

    Aceita não aceita - Placeholder

--- app/Views/Base_View.php
+++ app/Views/Base_View.php
@@ -3,28 +3,27 @@
 
 <head>
 
-<!-- dados da pagina -->
-<meta charset="UTF-8">
-<title>TuringMachine</title>
-<meta name="description" content="The small framework with powerful features">
-<meta name="viewport" content="width=device-width, initial-scale=1.0">
-<link rel="shortcut icon" type="image/png" href="/favicon.png"/>
+  <!-- dados da pagina -->
+  <meta charset="UTF-8">
+  <title>TuringMachine</title>
+  <meta name="description" content="The small framework with powerful features">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="shortcut icon" type="image/png" href="/favicon.png" />
 
-<!-- carregando bootstrap -->
-<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
-<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+  <!-- carregando bootstrap -->
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+  <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
 
-<!-- carregando Jquery -->
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+  <!-- carregando Jquery -->
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
 
-<!-- carregando FontAwesome -->
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.11.2/css/all.css">
+  <!-- carregando FontAwesome -->
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.11.2/css/all.css">
 
 
-<!-- switch -->
-<link href="https://cdn.jsdelivr.net/gh/gitbrent/bootstrap4-toggle@3.6.1/css/bootstrap4-toggle.min.css" rel="stylesheet">
-<script src="https://cdn.jsdelivr.net/gh/gitbrent/bootstrap4-toggle@3.6.1/js/bootstrap4-toggle.min.js"></script>
+  <!-- switch -->
+  <link href="https://cdn.jsdelivr.net/gh/gitbrent/bootstrap4-toggle@3.6.1/css/bootstrap4-toggle.min.css" rel="stylesheet">
+  <script src="https://cdn.jsdelivr.net/gh/gitbrent/bootstrap4-toggle@3.6.1/js/bootstrap4-toggle.min.js"></script>
 </head>
-
--- app/Views/Painel_Controle_View.php
+++ app/Views/Painel_Controle_View.php
@@ -3,155 +3,164 @@
 
 <?= $this->include('Base_View') ?>
 <?= $this->include('NavBar') ?>
+
 <body class="clearfix">
 
-<!-- CONTENT -->
-<div class= mt-4>
-    <?= $this->include('Tabs/Painel_controle_top') ?>
-</div>
+	<!-- CONTENT -->
 
-<div class="container-fluid">
-	<div class="row">
-		<div class="col-md-1">
-		</div>
-		<div class="col-md-10">
-			<form name="Controle" role="form" id="Controle">
-
-				<div class="form-group">					 
-                  <label for="Estados">Estados</label>
-                  <textarea name="Estados" class="form-control" id="Estados" placeholder="Estados"></textarea>
-				  <span class="help-block"></span>
+	<div class="container-fluid">
+		<div class="row">
+			<div class="col-md-1">
+			</div>
+			<div class="col-md-10">
+				<div class=mt-4>
+					<?= $this->include('Tabs/Tab_4') ?>
 				</div>
+				<form name="Controle" role="form" id="Controle">
 
-				<div class="row">
-                	<div class="col md-5 col-xs-12">
-                	  <label for="FitaEntrada">FitaEntrada</label>
-                	  <input name="FitaEntrada" class="form-control" id="FitaEntrada" placeholder="FitaEntrada">
-					  <span class="help-block"></span>
-					</div>
-	
-                	<div class="col-md-5 col-xs-12">
-                	  <label for="TamanhoFita">TamanhoFita</label>
-					  <input name="TamanhoFita" class="form-control" type="number" 
-								  min="20" max="10000" step="10" value="30" id="TamanhoFita" placeholder="TamanhoFita">
+					<div class="form-group">
+						<label for="Estados">Estados</label>
+						<textarea name="Estados" class="form-control" id="Estados" placeholder="Estados"></textarea>
 						<span class="help-block"></span>
-			    	</div>
-			    </div>
-
-			</form>
-            <button type="button" id="btnEnviar" onclick="Enviar()" class="btn btn-primary  mb-5 mt-4">Enviar</button>
-		</div>
-		<div class="col-md-1">
-		</div>
-	</div>
-</div>
-
-
-<div class="container-fluid">
-	<div class="row">
-		<div class="col-md-1">
-		</div>
-		<div class="col-md-10">
-			<div class="row">
-							
-				<div class="col-md-12 mt-5">
-
-					<div class="float-center">				
-						<h3>Saida de dados</h3>
 					</div>
-					<div class="float-right">
-						<input id="SaidaAceita" type="checkbox" checked data-toggle="toggle" data-on="aceita" data-off="Nao Aceita" data-onstyle="success" data-offstyle="danger">
+
+					<div class="row">
+						<div class="col md-5 col-xs-12">
+							<label for="FitaEntrada">FitaEntrada</label>
+							<input name="FitaEntrada" class="form-control" id="FitaEntrada" placeholder="FitaEntrada">
+							<span class="help-block"></span>
+						</div>
+
+						<div class="col-md-5 col-xs-12">
+							<label for="TamanhoFita">TamanhoFita</label>
+							<input name="TamanhoFita" class="form-control" type="number" min="20" max="10000" step="10" value="30" id="TamanhoFita" placeholder="TamanhoFita">
+							<span class="help-block"></span>
+						</div>
 					</div>
-					<textarea name="SaidaDados" class="form-control" id="SaidaDados" placeholder="SaidaDados" disabled></textarea>
 
-				</div>
-
+				</form>
+				<button type="button" id="btnEnviar" onclick="Enviar()" class="btn btn-primary  mb-5 mt-4">Enviar</button>
+			</div>
+			<div class="col-md-1">
 			</div>
 		</div>
-		<div class="col-md-1">
+	</div>
+
+
+	<div class="container-fluid">
+		<div class="row">
+			<div class="col-md-1">
+			</div>
+			<div class="col-md-10">
+				<div class="row">
+
+					<div class="col-md-12 mt-5">
+
+						<div class="float-center">
+							<h3>Saida de dados</h3>
+						</div>
+						<div class="float-right">
+							<!-- <input  type="checkbox" checked data-toggle="toggle" data-on="aceita" data-off="Nao Aceita" data-onstyle="success" data-offstyle="danger"> -->
+							<div class="information-container">
+								<span id="SaidaAceita" class="saida-text"></span>
+							</div>
+
+						</div>
+						<textarea name="SaidaDados" class="form-control" id="SaidaDados" placeholder="SaidaDados" disabled></textarea>
+
+					</div>
+
+				</div>
+			</div>
+			<div class="col-md-1">
+			</div>
 		</div>
 	</div>
-</div>
 
-<?= $this->include('Footer') ?>
+	<?= $this->include('Footer') ?>
 
 
-<!-- SCRIPTS -->
-<script type= 'text/javascript'>
+	<!-- SCRIPTS -->
+	<script type='text/javascript'>
+		function Enviar() {
+			$('#btnEnviar').text('Enviando...'); //change button text
+			$('#btnEnviar').attr('disabled', true); //set button disable 
+			var url;
 
-function Enviar()
-{
-    $('#btnEnviar').text('Enviando...'); //change button text
-    $('#btnEnviar').attr('disabled',true); //set button disable 
-    var url;
 
-   
-	url = "<?php echo site_url('Painel_Controle/Controle')?>";
- 
- 
-    $.ajax({
-        url : url,
-        type: "POST",
-        data: $('#Controle').serialize(),
-        dataType: "JSON",
-        success: function(data)
-        {
-			if(data.status)
-            {
-				fita ="";
-				for (var x in data[0][0]){
-					fita +=data[0][0][x]
-					fita +='\n'
-				}
-				
-				$('[name="SaidaDados"]').val(fita);
+			url = "<?php echo site_url('Painel_Controle/Controle') ?>";
 
-				//document.getElementById("SaidaAceita").disabled = false ;
-				document.getElementById("SaidaAceita").checked = false ;
-				//document.getElementById("SaidaAceita").disabled = true ;
-            }
-            else
-            {
-				// Houve falha na chamada ajax. Exibir mensagem de erro
-                $('#crudVeiculo .form-group').removeClass('has-error');
-                $('#crudVeiculo .help-block').text('');
-				$('#general-error').removeClass('bg-danger');
-				$('#general-error').text('');
-				if(data.inputerror)
-				{
-					// se foi um erro de validação de campos
-					for (var i = 0; i < data.inputerror.length; i++) 
-					{
-						$('[name="'+data.inputerror[i]+'"]').addClass('is-invalid');
-                        $('[name="'+data.inputerror[i]+'"]').next().text(data.error_string[i]);
+
+			$.ajax({
+				url: url,
+				type: "POST",
+				data: $('#Controle').serialize(),
+				dataType: "JSON",
+				success: function(data) {
+					if (data.status) {
+						fita = "";
+						for (var x in data[0][0]) {
+							fita += data[0][0][x]
+							fita += '\n'
+						}
+
+						$('[name="SaidaDados"]').val(fita);
+
+						document.getElementById("SaidaAceita").innerHTML = data[0][1] ? 'Aceita' : "Não aceita";
+						$('#SaidaAceita').addClass(data[0][1] ? 'aceita' : "nao-aceita")
+						$('.information-container').fadeIn("slow");
+						//document.getElementById("SaidaAceita").disabled = true ;
+					} else {
+						// Houve falha na chamada ajax. Exibir mensagem de erro
+						$('#crudVeiculo .form-group').removeClass('has-error');
+						$('#crudVeiculo .help-block').text('');
+						$('#general-error').removeClass('bg-danger');
+						$('#general-error').text('');
+						if (data.inputerror) {
+							// se foi um erro de validação de campos
+							for (var i = 0; i < data.inputerror.length; i++) {
+								$('[name="' + data.inputerror[i] + '"]').addClass('is-invalid');
+								$('[name="' + data.inputerror[i] + '"]').next().text(data.error_string[i]);
+							}
+						}
+						if (data.generalerror) {
+							// se foi um erro generalizado
+							$('#general-error').addClass('bg-danger');
+							$('#general-error').text(data.generalerror);
+						}
 					}
-				}
-				if(data.generalerror)
-				{
-					// se foi um erro generalizado
-					$('#general-error').addClass('bg-danger');
-					$('#general-error').text(data.generalerror);
-				}
-            }
- 
-            $('#btnEnviar').text('Enviar'); //change button text
-            $('#btnEnviar').attr('disabled',false); //set button enable 
- 
- 
-        },
-        error: function (jqXHR, textStatus, errorThrown)
-        {
-            alert('Error adding / update data');
-            $('#btnEnviar').text('Enviar'); //change button text
-            $('#btnEnviar').attr('disabled',false); //set button enable 
- 
-        }
-    });
-}
+
+					$('#btnEnviar').text('Enviar'); //change button text
+					$('#btnEnviar').attr('disabled', false); //set button enable 
 
 
-</script>
-<!-- -->
+				},
+				error: function(jqXHR, textStatus, errorThrown) {
+					alert('Error adding / update data');
+					$('#btnEnviar').text('Enviar'); //change button text
+					$('#btnEnviar').attr('disabled', false); //set button enable 
+
+				}
+			});
+		}
+	</script>
+	<!-- -->
+
+	<style type="text/css">
+		.saida-text {
+			font-size: 16px;
+			transition: all 1s;
+		}
+
+		.information-container {
+			padding: 8px 16px;
+			background-color: rgba(222, 222, 222, 0.5);
+			border-radius: 16px;
+			margin-bottom: 16px;
+			display: none;
+		}
+	</style>
 
 </body>
+
 </html>
