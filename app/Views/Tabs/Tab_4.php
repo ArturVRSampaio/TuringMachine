6cc0cc5222cfda561d41f2d54a307940a5d86d9a
Reinaldo Rozato Junior <naldoepingo1@gmail.com>
2020-11-12 23:10:09 -0300

    tutorial

--- app/Views/Tabs/Tab_4.php
+++ app/Views/Tabs/Tab_4.php
@@ -1,4 +1,30 @@
 <div>
-    <h3>Menu 4</h3>
-      <p>Eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.</p>
+    <h3>Utilização da máquina</h3>
+      <p>Esta Máquina de Turing tem o foco de ser facilmente tulizada por iniciantes, portanto abaixo se encontra os principais pontos de como utiliza-la.</p>
+      <img src="https://i.imgur.com/MS4CC5P.png" class="w-25 rounded mb-4 mt-4 mx-auto d-block" alt="Responsive image">
+      <div>
+          <b>1. Estado Atual:</b>
+          Estado em que a máquina está durante esta operação.
+          <p class="ml-3">Valores aceitos: 0 - &#x221e</p><br>
+
+          <b>2.Leitura Esperada:</b>
+          A leitura na qual a máquina está esperando para realizar o comando.<br>
+          <p class="ml-3">Valores aceitos: Qualquer caracter ou palavra desejada.</p><br>
+
+          <b>3.Escrita:</b>
+          Escreve o conteúdo desta parte do comando na fita.<br>
+          <p class="ml-3">Valores aceitos: Qualquer caracter ou palavra desejada.</p><br>
+
+          <b>4.Movimento:</b>
+          Para qual lado e quantas posições o cabeçote irá se mover.<br>
+          <p class="ml-3">Valores aceitos: Qualquer número real sendo que quaisquer negativos a máquina irá para a esquerda, zero fará a máquina não se mover e quaisquer número positivo fara a máquina se mover para a direita.
+          Este número também define a quantida de movimentos.</p><br>
+
+          <b>5.Próximo estado:</b>
+          Indica qual será o próximo estado da máquina.<br><br>
+
+          Este pequeno video mostra o funcionamento da máquina tendo como a fita [0,0]<br>
+
+          <iframe src='https://gfycat.com/ifr/MeanBlueKid' frameborder='0' scrolling='no' allowfullscreen width='640' height='404'></iframe>
+      </div>
 </div>
